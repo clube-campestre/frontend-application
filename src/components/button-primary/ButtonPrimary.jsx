8fce0852f68ed9b81e16f99a3eac9da18c3cb8bd
Moisés Silva <moises.silva@sptech.school>
2025-04-06 16:30:06 -0300

    FEAT :: Construção do compenente header/section1 e footer

--- src/components/button-primary/ButtonPrimary.jsx
+++ src/components/button-primary/ButtonPrimary.jsx
@@ -1,8 +1,8 @@
-const ButtonPrimary = ({text, onClick}) => {
+const ButtonPrimary = ({text, onClick, className}) => {
     return (
         <button
         onClick={onClick}
-        className="bg-[#FCAE2D] hover:bg-[#FECC4F] text-white font-bold py-1 px-8 rounded-2xl"
+        className={`bg-[#FCAE2D] hover:bg-[#FECC4F] text-black font-bold py-1 px-8 rounded-2xl cursor-pointer ${className || ''}`}
         >
         {text}
         </button>
--- src/pages/sections/header/Header.jsx
+++ src/pages/sections/header/Header.jsx
@@ -1,39 +1,64 @@
-import { useState } from "react";
 import ButtonPrimary from "../../../components/button-primary/ButtonPrimary";
+import desbravadoresHome from "../../../assets/images/desbravador-home.jpg";
+import Logo from "../../../assets/images/Logo.png";
 
 const Header = () => {
   return (
-    <section className="w-full h-view bg-gradient-to-b from-[#000000] to-[#00000000]">
-      <div className="flex justify-between items-center p-4 text-black">
+    <section className="w-full h-screen relative overflow-hidden" id="header">
+      <div className="absolute inset-0 z-0 clip-path-image">
+  <img
+    src={desbravadoresHome}
+    alt="Desbravadores"
+    className="w-full h-screen object-cover"
+  />
+
+<div className="absolute inset-0 bg-black/85 z-10" /> 
+</div>
+
+
+      <div className="relative z-20 flex justify-between items-center p-4">
         <figure>
-          <img src="/logo.png" alt="Logo" className="h-8" />
+          <img src={Logo} alt="Logo" className="h-16" />
         </figure>
         <nav className="space-x-20 text-base">
-          <a href="/" className="hover:text-gray-300">
+          <a href="/" className="text-white hover:text-gray-300">
             Início
           </a>
-          <a href="/about" className="hover:text-gray-300">
+          <a href="/about" className="text-white hover:text-gray-300">
             Sobre nós
           </a>
-          <a href="/contact" className="hover:text-gray-300">
+          <a href="/contact" className="text-white hover:text-gray-300">
             Unidades
           </a>
-          <a href="/services" className="hover:text-gray-300">
+          <a href="/services" className="text-white hover:text-gray-300">
             Classes
           </a>
-          <ButtonPrimary text="Entrar" onClick={() => console.log("Entrar")} />
+          <ButtonPrimary text="LOGIN" onClick={() => console.log("Login")} />
         </nav>
       </div>
-      <article className="flex flex-col items-center justify-center h-full w-view ">
-        <h1 className="text-4xl font-bold text-white mb-4 w-xl text-center">
-          Ser Desbravador Campestre é um chamado para servir, amar e
-          transformar!
+
+      <article className="relative z-20 flex flex-col items-center justify-center h-[60%] w-full">
+        <h1 className="text-5xl font-bold text-white mb-5 w-2/3 text-center leading-tight">
+          Ser <span className="text-[#FCAE2D]">Desbravador Campestre</span>
+          <br />é um chamado para servir,
+          <br />amar e transformar!
         </h1>
+      </article>
+
+      <div className="absolute z-30 left-1/2 transform -translate-x-1/2 bottom-[22%]">
+
         <ButtonPrimary
-          text="Saiba mais"
+          text="SAIBA MAIS"
           onClick={() => console.log("Saiba mais")}
         />
-      </article>
+      </div>
+
+      <style jsx>{`
+  .clip-path-image {
+    clip-path: polygon(0 0, 100% 0, 100% 70%, 50% 96%, 0 70%);
+  }
+`}</style>
+
     </section>
   );
 };
