dbdf117c0ca1e2dbf3a8f2a341318b26abb11011
RoniAndradee <ronielli.santos@sptech.school>
2025-04-07 11:31:33 -0300

    feat: adicionando seções About Us e Unidades ao componente Main; Subindo imagens otimizadas

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,12 +1,15 @@
 import Header from "../sections/header/header";
 import Footer from "../sections/Footer.jsx"
-
+import AboutUs from "../sections/about-us/AboutUs.jsx"
+import Unities from "../sections/unities/Unities.jsx"
 
 const Main = () => {
     return (
         <>
         <Header/>
-        <Footer></Footer>
+        <AboutUs/>
+        <Unities/>
+        <Footer/>
         </>
     );
 }
