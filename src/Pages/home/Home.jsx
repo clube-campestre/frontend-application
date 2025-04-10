553422850eb08906d4669f96f93d52162b793524
RoniAndradee <ronielli.santos@sptech.school>
2025-04-10 14:01:10 -0300

    creating sidebar component

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -2,14 +2,16 @@ import Header from "../sections/header/header.jsx";
 import Footer from "../sections/Footer.jsx"
 import AboutUs from "../sections/about-us/AboutUs.jsx"
 import Unities from "../sections/unities/Unities.jsx"
+import Sidebar from "../../components/side-bar/SideBar.jsx";
 
 const Home = () => {
     return (
         <>
-        <Header/>
+        {/* <Header/>
         <AboutUs/>
         <Unities/>
-        <Footer/>
+        <Footer/> */}
+        <Sidebar activePath={"/admin"}/>
         </>
     );
 }
--- src/Pages/sections/Footer.jsx
+++ src/Pages/sections/Footer.jsx
@@ -91,5 +91,6 @@ const Footer = () => {
 			</div>
 		</footer>
 	);
+};
 
 export default Footer;
