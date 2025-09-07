16009dd1c69bdebdded26c35c8d8b146b2bd06ea
yEllenCaroline <ellen.chaves@sptech.school>
2025-09-07 12:31:47 -0300

    Feat: Esqueci minha senha adicionado

--- src/services/userService.js
+++ src/services/userService.js
@@ -114,3 +114,75 @@ export const registerService = async (
 		return false;
 	}
 };
+
+export const forgotPasswordService = async (email) => {
+    try {
+        const response = await api.post("/auth/forgot-password", null, {
+            params: { email }
+        });
+        Swal.fire({
+            title: "Código enviado!",
+            text: "Verifique seu e-mail para o código de recuperação.",
+            icon: "success",
+            confirmButtonColor: "#FCAE2D",
+        });
+        return response.data;
+    } catch (error) {
+        Swal.fire({
+            title: "Erro ao enviar código!",
+            text: "E-mail não cadastrado ou inserido incorretamente.	 Tente novamente.",
+            icon: "error",
+            confirmButtonColor: "#FCAE2D",
+        });
+        console.error(error);
+        return null;
+    }
+};
+
+export const verifyCodeService = async (email, code) => {
+    try {
+        const response = await api.post("/auth/verify-code", null, {
+            params: { email, code }
+        });
+		Swal.fire({
+            title: "Código verificado!",
+            text: "Você pode agora definir uma nova senha.",
+            icon: "success",
+            confirmButtonColor: "#FCAE2D",
+        });
+        return response.data;
+    } catch (error) {
+        Swal.fire({
+            title: "Código inválido!",
+            text: "O código informado está incorreto ou expirado.",
+            icon: "error",
+            confirmButtonColor: "#FCAE2D",
+        });
+        console.error(error);
+        return null;
+    }
+};
+
+export const resetPasswordService = async (email, code, newPassword) => {
+    try {
+        const response = await api.post("/auth/reset-password", null, {
+            params: { email, code, newPassword }
+        });
+        Swal.fire({
+            title: "Senha redefinida!",
+            text: "Sua senha foi alterada com sucesso.",
+            icon: "success",
+            confirmButtonColor: "#FCAE2D",
+        });
+        return response.data;
+    } catch (error) {
+        Swal.fire({
+            title: "Erro ao redefinir senha!",
+            text: "Não foi possível redefinir a senha. Tente novamente.",
+            icon: "error",
+            confirmButtonColor: "#FCAE2D",
+        });
+        console.error(error);
+        return null;
+    }
+};
