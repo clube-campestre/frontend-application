60a723550061cb556b51aa0b708854e1320247cd
yEllenCaroline <ellen.chaves@sptech.school>
2025-10-30 01:18:06 -0300

    feat: Rotas funcionais!

--- src/components/admin-internal/FormRegister.jsx
+++ src/components/admin-internal/FormRegister.jsx
@@ -77,6 +77,7 @@ const FormRegister = ({ formTitle, fields, onSubmit }) => {
       telefone: unmaskPhone(formData.telefone),
       whatsapp: unmaskPhone(formData.whatsapp),
       // cep: formData.cep.replace(/\D/g, ""),
+      
     };
 
     onSubmit(updatedFormData);
